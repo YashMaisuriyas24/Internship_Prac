b964ffd145ca507d54b4d69641e808c8ccd01877
YashMaisuriyas24 <yashmaisuriyas04@gmail.com>
2026-04-01 17:57:48 +0530

    implemented langchain HUmanInTheLoop Middleware

--- 31-03-2026/Langchain_HumanInTheLoop_Middleware/main.py
+++ 31-03-2026/Langchain_HumanInTheLoop_Middleware/main.py
@@ -7,6 +7,7 @@ from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from langchain_mongodb import MongoDBChatMessageHistory
 from pymongo import MongoClient
 from langgraph.checkpoint.memory import InMemorySaver
+from langchain_core.tools import tool
 
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
@@ -24,6 +25,8 @@ session_id = "user_session_1"
 mongo_memory = MongoDBChatMessageHistory(
     connection_string=MONGO_URI,
     session_id=session_id,
+    database_name="chat_memory1",
+    collection_name="history1"
 )
 
 SYSTEM_PROMPT = """
@@ -42,10 +45,12 @@ Guidelines:
 
 @tool
 def your_read_email_tool(email_id: str) -> str:
+    """Mock function to read an email by its ID."""
     return f"Email content for ID: {email_id}"
 
 @tool
 def your_send_email_tool(recipient: str, subject: str, body: str) -> str:
+    """Mock function to send an email."""
     return f"Email sent to {recipient} with subject '{subject}'"
 
 agent = create_agent(
